8f2e521dfd5c686383afb5d6ae932a812da459c9
Md. Mamun Hossain <muhammed.mamun.131@gmail.com>
2024-12-18 13:09:43 +0600

    input validation types is working

--- chatverse/apps/http/src/index.ts
+++ chatverse/apps/http/src/index.ts
@@ -1,18 +1,17 @@
 import express from "express"
 
+import client from "@repo/db/client"
+
 const app = express()
+app.use(express.json())
 
-// app.use("/api/v1")
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "hello from localhost"
-    })
-})
+app.use("/api/v1")
+
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-    console.log(`http://localhost:${port}`);
+    console.log(`http://localhost:${port}/api/v1`);
 })
--- chatverse/apps/http/src/router/v1/index.ts
+++ chatverse/apps/http/src/router/v1/index.ts
@@ -1,5 +1,20 @@
 import { Router } from "express";
-
+import { authRouter } from "./auth";
+import { userRouter } from "./user";
+import { adminRouter } from "./admin";
+import { spaceRouter } from "./space";
 
 export const router = Router()
 
+router.get("/elements", (req, res) => {
+    
+})
+
+router.get("/avaters", (req, res) => {
+    
+})
+
+router.use("/auth", authRouter)
+router.use("/user", userRouter)
+router.use("/admin", adminRouter)
+router.use("/space", spaceRouter)
